c583a6e2a158581fc7ef902431393d4dfeda2afd
RPGILL <R.GILL3@RGU.AC.UK>
2025-02-26 18:28:41 +0000

    change

--- server.js
+++ server.js
@@ -69,6 +69,6 @@ app.get("/get-users", (req, res) => {
 
 // ✅ Start Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
 });
